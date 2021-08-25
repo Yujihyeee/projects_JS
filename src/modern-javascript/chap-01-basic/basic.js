837ce565ca2685ac08c8c7681d8315e1b5e8f3d2
Yujihyeee <rladnddl@naver.com>
2021-08-25 10:18:51 +0900

    10

---
+++ src/modern-javascript/chap-01-basic/basic.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+var man = {} // es5
+
+
+const Basic = () => {
+    let tom = 'Tom' // es6에서 variable
+    const james = 'James' // es6에서 constant
+    return(<></>)
+
+}
+
+export default Basic
---
+++ src/modern-javascript/chap-02-arrow-function/arrow-function.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ArrowFunction = () => {
+    return(<></>)
+}
+
+export default ArrowFunction
