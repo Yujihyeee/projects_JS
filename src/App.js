ccd35f02a5c42116e058adea6e4ccda7d119825f
Yujihyeee <rladnddl@naver.com>
2021-08-30 15:50:26 +0900

    15

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React , {component} from 'react';
 import {Route, Redirect, Switch} from 'react-router-dom'
-import {CourseRegister, OnlineProfile, Join, Login, SchoolStatus, ImageMap, Anchor, DateMonth, NumberRange, Color} from './components/index'
-import Home from './components/Home';
-import Navigation from './components/Navigation';
+import {CourseRegister, OnlineProfile, Join, Login, SchoolStatus, ImageMap, Anchor, DateMonth,NumberRange, 
+        Color, Navigation, NavigationForJavaScript, Header} from './components/index'
 import styled from 'styled-components';
-import Header from './components/Header';
-import NavigationForJavaScript from './components/NavigatiomForJavaScript';
-import Basic from './modern-javascript/chap-01-basic/Basic'
-import ArrowFunction from './modern-javascript/chap-02-arrow-function/ArrowFunction'
-import Arguments from './modern-javascript/chap-03-arguments/Arguments'
-import TemplateLiteral from './modern-javascript/chap-04-template-literal/TemplateLiteral'
-import LiteralMethod from './modern-javascript/chap-05-literal-method/LiteralMethod'
-import Destructuring from './modern-javascript/chap-06-destucturing/Destructuring'
-import Loop from './modern-javascript/chap-07-loop/Loop'
-import ArrayMethod from './modern-javascript/chap-08-array-method/ArrayMethod'
-import SpreadOperatorRestParameter from './modern-javascript/chap-09-spread-oprator-rest-parameter/SpreadOperatorRestParameter'
-import ObjectLiteral from './modern-javascript/chap-10-object-literal/ObjectLiteral'
-import Symbol from './modern-javascript/chap-11-symbol/Symbol'
-import Class from './modern-javascript/chap-12-class/Class'
-import Promise from './modern-javascript/chap-13-promise/Promise'
-import Generator from './modern-javascript/chap-14-generator/Generator'
-import Proxy from './modern-javascript/chap-15-proxy/Proxy'
-import SetMap from './modern-javascript/chap-16-set-map/SetMap'
-import Array from './modern-javascript/chap-17-array/Array'
-import ES2017 from './modern-javascript/chap-18-es2017/ES2017'
-import AsnycAwait from './modern-javascript/chap-19-async-await/AsnycAwait'
-import ES2018 from './modern-javascript/chap-20-es2018/ES2018'
-import ES2019 from './modern-javascript/chap-21-es2019/ES2019'
-import ES2020 from './modern-javascript/chap-22-es2020/ES2020'
-import TypeScript from './modern-javascript/chap-23-typescript/TypeScript'
+import {Basic, ArrowFunction, Arguments, TemplateLiteral, LiteralMethod, Destructuring, Loop, ArrayMethod, 
+        SpreadOperatorRestParameter, ObjectLiteral, Symbol, Class, Promise, Generator, Proxy, SetMap, Array, 
+        ES2017, AsnycAwait, ES2018, ES2019, ES2020, TypeScript} from './modern-javascript/index'
 import HomePage from './pages/HomePage';
-
 
 
 const App = () => (
